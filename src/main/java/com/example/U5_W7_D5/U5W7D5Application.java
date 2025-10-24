07ffed080005dc6a845c7520711e48e4627c9560
Valerio Di Felice <valesmile.99@gmail.com>
2025-10-24 09:58:40 +0200

    Initialize Spring Boot project with Maven
    Add initial Spring Boot application setup with Maven wrapper, project structure, dependencies, and a basic test. Includes .gitignore, pom.xml, application properties, and main application class.

---
+++ src/main/java/com/example/U5_W7_D5/U5W7D5Application.java
@@ -0,0 +1,13 @@
+package com.example.U5_W7_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W7D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W7D5Application.class, args);
+	}
+
+}
